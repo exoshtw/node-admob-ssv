cc8ed48379891b7d16791fb8fca070335c133660
exos <exos@kaktheplanet.xyz>
2020-04-30 16:44:41 -0300

    Add index

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+
+import Verifier from './verifier';
+import MemoryCache from './keydict/cache/memory';
+import KeyProvider from './keydict/provider';
+
+export default Verifier;
+export {MemoryCache};
+export {KeyProvider};
